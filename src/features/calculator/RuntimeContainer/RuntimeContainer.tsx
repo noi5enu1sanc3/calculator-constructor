4fc8c25e7d7cba8853cab8b71fcbb6a3e0250f58
noisenuisance <sabauspne@gmail.com>
2023-03-13 02:35:12 +0300

    add easter egg

--- src/features/calculator/RuntimeContainer/RuntimeContainer.tsx
+++ src/features/calculator/RuntimeContainer/RuntimeContainer.tsx
@@ -1,3 +1,5 @@
+import cx from 'classnames';
+
 import ConstructorElement from '../../../shared/Blocks/ConstructorElement/ConstructorElement';
 import { BlockId } from '../../dnd/utils/constants';
 import { useCalculator } from '../hooks/useCalculator';
@@ -12,7 +14,7 @@ function RuntimeContainer({ blockIds }: Props) {
   const { displayValue, handleClick, isError } = useCalculator();
 
   return (
-    <div onClick={handleClick} className={styles.container}>
+    <div onClick={handleClick} className={cx(styles.container, { [styles.empty]: blockIds.length === 0 })}>
       {blockIds.map((id) => (
         <ConstructorElement key={id} id={id} value={displayValue} isRuntime={true} isError={isError} />
       ))}
